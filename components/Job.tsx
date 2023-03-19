ff881b3cf3628f7cb7c17d9f07ee4f6d822063b4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-19 22:58:17 +0530

    Converts unix time to a formatted date

--- components/Job.tsx
+++ components/Job.tsx
@@ -17,7 +17,14 @@ export function Job({ state }: { state: string }) {
     <div className="border border-purple-600 my-4 p-4 w-8/12">
       <section className="flex flex-col gap-2 shadow-lg px-6 py-5 rounded-lg">
         <h1 className="text-lg font-semibold">{title}</h1>
-        <p>Posted on: {date}</p>
+        <p>
+          Posted on{" "}
+          {new Date().toLocaleDateString("en-US", {
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+          })}
+        </p>
         <div className="flex items-baseline justify-start gap-3">
           <a
             href={jobUrl}
