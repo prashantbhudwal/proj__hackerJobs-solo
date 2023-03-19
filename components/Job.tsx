8f5d2547ee794e746f7c522ca0ee0cb1180b866d
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-19 22:50:05 +0530

    Add base styles for the card

--- components/Job.tsx
+++ components/Job.tsx
@@ -14,14 +14,23 @@ export function Job({ state }: { state: string }) {
   const { id, url: jobUrl, title, time: date } = jobData;
 
   return (
-    <section className="border border-purple-600 my-4 p-4">
-      <h1>{title}</h1>
-      <p>Posted on: {date}</p>
-      <div>
-        <a href={jobUrl}>Apply Now</a>
-        <button>{state === "added" ? "Saved" : "Save for Later"}</button>
-      </div>
-    </section>
+    <div className="border border-purple-600 my-4 p-4 w-8/12">
+      <section className="flex flex-col gap-2 shadow-lg px-6 py-5 rounded-lg">
+        <h1 className="text-lg font-semibold">{title}</h1>
+        <p>Posted on: {date}</p>
+        <div className="flex items-baseline justify-start gap-3">
+          <a
+            href={jobUrl}
+            className=" bg-red-600 text-white rounded-sm px-2 py-2 w-32 text-center"
+          >
+            Apply Now
+          </a>
+          <button className="bg-purple-600 text-white rounded-sm px-2 py-2 w-32">
+            {state === "added" ? "Saved" : "Save for Later"}
+          </button>
+        </div>
+      </section>
+    </div>
   );
 }
 
