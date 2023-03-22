c65db7d7963ff90f17d3f1e824de4245b95e1670
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-22 17:19:54 +0530

    Render all JobIds

--- components/Job.tsx
+++ components/Job.tsx
@@ -1,38 +1,35 @@
-export default function Job({
-  state,
-  jobData,
-}: {
-  state: string;
-  jobData: any;
-}) {
-  const { id, url: jobUrl, title, time: date } = jobData;
+export default function Job({ state, jobId }: { state: string; jobId: any }) {
+  // const jobData = useJobData()
+  // const { id, url: jobUrl, title, time: date } = jobData;
+
   return (
-    <section className="flex flex-col gap-3 shadow-lg px-6 py-5 rounded-lg">
-      <h1 className="text-lg font-semibold">{title}</h1>
-      <p>
-        Posted on{" "}
-        {new Date(date * 1000).toLocaleDateString("en-US", {
-          day: "numeric",
-          month: "short",
-          year: "numeric",
-        })}
-      </p>
-      <div className="flex items-baseline justify-start gap-3">
-        <a
-          href={jobUrl}
-          className=" bg-red-600 text-white rounded-sm px-2 py-2 w-32 text-center"
-        >
-          Apply Now
-        </a>
-        <button
-          className={`bg-purple-600 text-white rounded-sm px-2 py-2 w-32 ${
-            state === "added" ? "bg-gray-600" : "bg-purple-600"
-          }`}
-        >
-          {state === "added" ? "Saved" : "Save for Later"}
-        </button>
-      </div>
-    </section>
+    <div>{JSON.stringify(jobId)}</div>
+    // <section className="flex flex-col gap-3 shadow-lg px-6 py-5 rounded-lg">
+    //   <h1 className="text-lg font-semibold">{title}</h1>
+    //   <p>
+    //     Posted on{" "}
+    //     {new Date(date * 1000).toLocaleDateString("en-US", {
+    //       day: "numeric",
+    //       month: "short",
+    //       year: "numeric",
+    //     })}
+    //   </p>
+    //   <div className="flex items-baseline justify-start gap-3">
+    //     <a
+    //       href={jobUrl}
+    //       className=" bg-red-600 text-white rounded-sm px-2 py-2 w-32 text-center"
+    //     >
+    //       Apply Now
+    //     </a>
+    //     <button
+    //       className={`bg-purple-600 text-white rounded-sm px-2 py-2 w-32 ${
+    //         state === "added" ? "bg-gray-600" : "bg-purple-600"
+    //       }`}
+    //     >
+    //       {state === "added" ? "Saved" : "Save for Later"}
+    //     </button>
+    //   </div>
+    // </section>
   );
 }
 
