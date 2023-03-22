21807a81af1983d90b324992fcad18d584a488e0
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-22 12:25:42 +0530

    Add a Header

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center w-full h-16 px-6 bg-white shadow-md">
+      <h1 className="text-2xl font-semibold">Hacker Jobs</h1>
+    </header>
+  );
+}
