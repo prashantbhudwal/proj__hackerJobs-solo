d8e36eda9450080161f4d89899878d0fcb0209ca
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-23 19:26:24 +0530

    Add saved job count to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,17 @@
+"use client";
+import WishlistContext from "@/app/context/WishlistContext";
 import Link from "next/link";
-
+import { useContext } from "react";
 export default function Header() {
+  const { wishlist } = useContext(WishlistContext);
   return (
     <header className="flex justify-between items-center w-full h-16 px-6 bg-white shadow-md">
       <h1 className="text-2xl font-semibold">
         <Link href="/">Hacker Jobs</Link>
       </h1>
-      <Link href="/wishlist"> Wishlist</Link>
+      <Link href="/wishlist" className="underline underline-offset-4 text-lg font-semibold text-green-600">
+        {wishlist.length} Jobs in Wishlist
+      </Link>
     </header>
   );
 }
