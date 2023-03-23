06d94f48197c984a6c56559e114d3dd192fb8357
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-23 15:21:40 +0530

    Add wishlist feature to the app

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import Stack from "@/components/Stack";
 import Job from "@/components/Job";
 import useAllHNJobs from "./hooks/useAllHNJobs";
 import useHNJob from "./hooks/useHNJob";
+import useWishlist from "./hooks/useWishlist";
 export default function Home() {
+  const { wishlist, addToWishlist, removeFromWishlist, isInWishList } =
+    useWishlist();
+
   const [allJobs, error, isLoading] = useAllHNJobs();
   if (error) return <h1>Error</h1>;
   if (isLoading) return <h1></h1>;
@@ -11,7 +15,15 @@ export default function Home() {
   return (
     <Stack>
       {allJobs.map((jobId: number) => (
-        <Job jobId={jobId} state="added" key={jobId} jobFetcher={useHNJob} />
+        <Job
+          jobId={jobId}
+          key={jobId}
+          jobFetcher={useHNJob}
+          clickHandler={
+            isInWishList(jobId) ? removeFromWishlist : addToWishlist
+          }
+          inWishlist={isInWishList}
+        />
       ))}
     </Stack>
   );
