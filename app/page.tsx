11f9a1887092671a289f51b6a5d79ea3672c3c6e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-23 08:57:02 +0530

    Refactor JobStack

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import useSWR from "swr";
-import HackerJobs from "./jobs/page";
-
+import JobStack from "@/components/JobStack";
 export default function Home() {
   //  Used SWR because of some issue with native fetch in server components
   const fetcher = function () {
@@ -14,8 +13,8 @@ export default function Home() {
     error,
     isLoading,
   } = useSWR(`https://hacker-news.firebaseio.com/v0/jobstories.json`, fetcher);
-  if (error)  return <h1>Error</h1>;
-  if (isLoading)  return <h1>Loading</h1>;
+  if (error) return <h1>Error</h1>;
+  if (isLoading) return <h1>Loading</h1>;
 
-  return <HackerJobs allJobs={allJobs} />;
+  return <JobStack allJobs={allJobs} />;
 }
--- components/JobStack.tsx
+++ components/JobStack.tsx
@@ -1,3 +1,20 @@
-export default function JobStack({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col gap-4 max-w-xl">{children}</div>;
+import Job from "@/components/Job";
+const jobData = {
+  by: "aisrael",
+  id: 35154599,
+  score: 1,
+  time: 1678813241,
+  title: "PropelAuth (YC W22) Is Hiring Engineers",
+  type: "job",
+  url: "https://www.ycombinator.com/companies/propelauth/jobs",
+};
+
+export default function JobStack({ allJobs }: { allJobs: number[] }) {
+  return (
+    <div className="flex flex-col gap-4 max-w-xl">
+      {allJobs.map((jobId: number) => (
+        <Job jobId={jobId} state="added" key={jobId} />
+      ))}
+    </div>
+  );
 }
