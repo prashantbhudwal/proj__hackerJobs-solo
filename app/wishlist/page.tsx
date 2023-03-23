2b0b3be2a00fb8c8d94aced2e0400cf31b92f825
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-23 19:54:14 +0530

    Add boilerplate for wishlist component

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,23 +1,27 @@
-import JobStack from "@/components/JobStack";
+"use client";
+import Stack from "@/components/Stack";
 import Job from "@/components/Job";
-export default function WishlistPage() {
-  const jobData = {
-    by: "aisrael",
-    id: 35154599,
-    score: 1,
-    time: 1678813241,
-    title: "PropelAuth (YC W22) Is Hiring Engineers",
-    type: "job",
-    url: "https://www.ycombinator.com/companies/propelauth/jobs",
-  };
+import { useContext } from "react";
+import WishlistContext from "../context/WishlistContext";
+export default function Wishlist() {
+  
+const { wishlist, addToWishlist, removeFromWishlist, isInWishList } =
+  useContext(WishlistContext);
+
+
   return (
-    <>
-      Wishlist
-      <JobStack>
-        <Job jobData={jobData} state="added" />
-        <Job jobData={jobData} state="added" />
-        <Job jobData={jobData} state="added" />
-      </JobStack>
-    </>
+    <Stack>
+      {/* {wishlist.map((jobData: JobData) => (
+        <Job
+          jobId={jobData.id}
+          key={jobData.id}
+          jobFetcher={useHNJob}
+          clickHandler={
+            isInWishList(jobData.id) ? removeFromWishlist : addToWishlist
+          }
+          inWishlist={isInWishList}
+        />
+      ))} */}
+    </Stack>
   );
 }
