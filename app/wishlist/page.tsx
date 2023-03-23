ba1034c5f7059b9356e333160179299ae77e1791
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-23 20:15:06 +0530

    Add a wishlist route

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -4,24 +4,25 @@ import Job from "@/components/Job";
 import { useContext } from "react";
 import WishlistContext from "../context/WishlistContext";
 export default function Wishlist() {
-  
-const { wishlist, addToWishlist, removeFromWishlist, isInWishList } =
-  useContext(WishlistContext);
-
+  const {
+    wishlist: jobData,
+    addToWishlist,
+    removeFromWishlist,
+    isInWishList,
+  } = useContext(WishlistContext);
 
   return (
     <Stack>
-      {/* {wishlist.map((jobData: JobData) => (
+      {jobData.map((jobData: JobData) => (
         <Job
-          jobId={jobData.id}
           key={jobData.id}
-          jobFetcher={useHNJob}
+          jobData={jobData}
           clickHandler={
             isInWishList(jobData.id) ? removeFromWishlist : addToWishlist
           }
           inWishlist={isInWishList}
         />
-      ))} */}
+      ))}
     </Stack>
   );
 }
